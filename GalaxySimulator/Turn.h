14ae993f6a4c9e884590316897a276bf0231f991
Chase Dorsey <cdorsey@bgsu.edu>
2016-02-22 12:40:43 -0500

    Added property values

--- GalaxySimulator/Turn.h
+++ GalaxySimulator/Turn.h
@@ -1,10 +1,23 @@
 #pragma once
 class Turn
 {
+private:
+   int planetaryNeb;
+   int TISupernova;
+   int TIISuperNova;
+   int num;
 public:
+   Turn():
+      planetaryNeb(0),
+      TISupernova(0),
+      TIISuperNova(0),
+      num(0)
+   {}
 
-   Turn()
-   {
-   }
+   Turn(int pn, int tI, int tII):
+      planetaryNeb(pn),
+      TISupernova(tI),
+      TIISuperNova(tII)
+   {}
 };
 
