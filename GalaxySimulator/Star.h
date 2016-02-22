163b6eccbee7f1a4c3bb00ef87ad06599c77736a
Chase Dorsey <cdorsey@bgsu.edu>
2016-02-22 12:40:25 -0500

    Added property values and getters and setters

--- GalaxySimulator/Star.h
+++ GalaxySimulator/Star.h
@@ -4,15 +4,35 @@
 class Star
 {
 private:
+   int starType;
    Turn *created;
 public:
+   const static int TYPE_ISM = 0;
+   const static int TYPE_HIGH_MASS = 1;
+   const static int TYPE_MED_MASS = 2;
+   const static int TYPE_LOW_MASS = 3;
 
-   Star()
+   Star():
+      starType(0),
+      created(nullptr)
+   { }
+
+   Star(int type, Turn* turn) :
+      starType(type),
+      created(turn)
+   { }
+
+   void setType(int type)
    {
+      this->starType = type;
    }
-
-   virtual ~Star()
+   void setCreated(Turn* turn)
    {
+      this->created = turn;
+   }
+   int getType() const
+   {
+      return this->starType;
    }
 };
 
